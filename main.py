c5dda871092cd1144813218b1d5db2791360193d
Nongrenost <nongrenost@gmail.com>
2024-12-16 18:53:46 +0300

    lint and refactor

--- main.py
+++ main.py
@@ -2,21 +2,20 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_text(book_path)
     word_count = get_word_count(text)
-    output(text, word_count)
+    output(word_count)
 
 def get_text(path):
     with open(path) as f:
         return f.read()
 
-def output(text, word_count):
+def output(word_count):
     print(f"Word count: {word_count}")
-    
+
+# remove if?
 def get_word_count(text):
     if text is None:
         return None
-    else:
-        _word_count = len(text.split())
-        return _word_count
+    _word_count = len(text.split())
+    return _word_count
 
-
-main()  
+main()
