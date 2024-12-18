b63ce29218a81ca845ce74ad6acbe6a5d760d7a4
Nongrenost <nongrenost@gmail.com>
2024-12-18 14:07:37 +0300

    finished output formatting

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ def main():
     text = get_text(book_path)
     word_count = get_word_count(text)
     letter_count = count_letters(text)
-    output(word_count, letter_count)
+    letters_sorted_only_alpha = get_only_alpha_list_sorted(letter_count)
+    output(word_count, letters_sorted_only_alpha, book_path)
+
 
 def get_text(path):
     with open(path) as f:
         return f.read()
 
-def output(word_count, letter_count):
-    print(f"Word count: {word_count}")
-    print(letter_count)
+def output(word_count, letters, path):
+    print(f"--- Begin report of {path} ---")
+    print(f"Word count: {word_count}\n")
+    
+    for record in letters:
+        print(f"Character {record["name"]} was found {record["value"]} times")
+    print("--- End report ---")
 
-# remove if?
 def get_word_count(text):
-    if text is None:
-        return None
     _word_count = len(text.split())
     return _word_count
 
@@ -29,5 +32,19 @@ def count_letters(text):
         else:
             counted_letters[lowered_char] = 1
     return counted_letters
+
+
+
+def sort_on(dict):
+    return dict["value"]
+
+def get_only_alpha_list_sorted(dict):
+    only_alpha_list = []
+    for record in dict:
+        if record[0].isalpha():
+            new_record = {"name":record, "value":dict[record]}
+            only_alpha_list.append(new_record)
+    only_alpha_list.sort(reverse=True, key=sort_on) 
+    return only_alpha_list 
 
 main()
